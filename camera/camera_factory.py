ad019383b1f3690eb2725ed6ebc28292956c8fab
Armando Ruz <armandx.18@gmail.com>
2025-05-25 23:40:19 -0400

    se modifican archivos /camera para poder visualizar multiples camaras

--- camera/camera_factory.py
+++ camera/camera_factory.py
@@ -1,11 +1,8 @@
-import os
 from camera.usb_camera import UsbCamera
 from camera.ip_camera import IpCamera
 
-def get_camera():
-    camera_type = os.getenv("CAMERA_TYPE", "USB").upper()
-    if camera_type == 'IP':
-        print("Camara IP cargada...")
-        return IpCamera()
-    print("Camara USB cargada...")
-    return UsbCamera()
+def create_camera_instance(config):
+    if config["type"] == "IP":
+        return IpCamera(config["url"])
+    elif config["type"] == "USB":
+        return UsbCamera(config["index"])
--- camera/usb_camera.py
+++ camera/usb_camera.py
@@ -1,7 +1,9 @@
-
 import cv2
 from camera.base import BaseCamera
 
 class UsbCamera(BaseCamera):
+    def __init__(self, index):
+        self.index = index
+
     def get_capture(self):
-        return cv2.VideoCapture(0)
+        return cv2.VideoCapture(self.index)
